26d9d421a0e459fc7dd742d825578432344a3672
Paul Morris <pmorris2012@fau.edu>
2022-06-07 05:16:23 -0400

    model fns

--- clip/model.py
+++ clip/model.py
@@ -66,6 +66,7 @@ def CLIP_RESBLOCK(params, x, heads: int, mask=None, name=''):
 
 def CLIP_TRANSFORMER(params, x, layers: int, heads: int, mask=None, name=''):
     for i in range(layers):
+        print(jnp.linalg.norm(x[0] - x[1]))
         x = CLIP_RESBLOCK(params, x, heads, mask=mask, name=name + F'.resblocks.{i}')
     return x
 
